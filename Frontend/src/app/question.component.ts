ce3f1fe569cdbaf39973175f238d9a4fcae49579
Usarov Sherzod <usarovsherzod68@gmail.com>
2019-06-30 23:25:31 +0500

    added new files

---
+++ Frontend/src/app/question.component.ts
@@ -0,0 +1,14 @@
+import{Component} from '@angular/core';
+
+@Component ({
+    templateUrl:  './question.component.html',
+    selector:'question'
+
+})
+
+export class QuestionComponent {
+    question:string;
+    post(){
+        console.log(this.question);
+    }
+}
